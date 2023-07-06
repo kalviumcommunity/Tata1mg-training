34aec4e8f8422790165e5df46b54af686f8c22f8
Sarthak Srivastava <sarthaksri2003@gmail.com>
2023-07-06 15:01:04 +0530

    3 questions practised in Python

--- Practice/Sarthak/PythonPract1.py
+++ Practice/Sarthak/PythonPract1.py
@@ -6,3 +6,59 @@ class Solution:
             if ans == n:
                 return True
          return False
+    
+
+#Solution to the "Reverse Integer" question from Leetcode in Python
+class Solution:
+    def reverse(self, x: int) -> int:
+        is_negative = x < 0
+        x = abs(x)
+        ans = 0
+        while x!= 0:
+            dig = x%10
+            if ans > (2147483647 // 10) or ans < (-(2147483648) // 10):
+                return 0
+
+            ans = (ans*10) + dig
+            x = x//10
+
+        if is_negative:
+            ans = -ans
+
+        return ans
+    
+#Solution to "Truncate sentence" problem
+class Solution:
+    def truncateSentence(self, s: str, k: int) -> str:
+        space = 0
+        i = 0
+        n = len(s)
+        re = ""
+        
+        while i != n:
+            if s[i] == ' ':
+                space += 1
+                
+            if space == k:
+                break
+            
+            re += s[i]
+            i += 1
+        
+        return re
+    
+#Solution to "Valid Anagram" problem
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        n = len(s)
+        m = len(t)
+        if n != m:
+            return False
+
+        sorted_s = sorted(s)
+        sorted_t = sorted(t)
+
+        if sorted_s == sorted_t:
+            return True
+
+        return False
